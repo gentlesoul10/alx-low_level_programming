b5cdec3f47d58f02a70c249967b66c1ebdac0852
gentlesoul10 <olivekelvin139@gmail.com>
2023-09-12 16:06:06 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - program that prints _putchar, followed by a new line.
+ * written by Olive
+ * Return: 0 for successful execution
+ */
+
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
+}
