4a4e3b8f075367208a6d7a47eaeaa1280edea5bb
Gentlesoul10 <olivekelvin139@gmail.com>
2023-10-24 21:29:28 -0700

    removed lines

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -14,8 +14,8 @@ size_t print_listint_safe(const listint_t *head);
  */
 size_t looped_listint_len(const listint_t *head)
 {
- const listint_t *tortoise, *hare;
- size_t nodes = 1;
+const listint_t *tortoise, *hare;
+size_t nodes = 1;
 
 if (head == NULL || head->next == NULL)
 return (0);
