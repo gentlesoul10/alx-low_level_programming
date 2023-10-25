0bac25b81f677156e281fb9cbbc0bc8950a8fa46
Gentlesoul10 <olivekelvin139@gmail.com>
2023-10-24 21:26:58 -0700

    removed lines

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -17,37 +17,37 @@ size_t looped_listint_len(const listint_t *head)
  const listint_t *tortoise, *hare;
  size_t nodes = 1;
 
- if (head == NULL || head->next == NULL)
- return (0);
+if (head == NULL || head->next == NULL)
+return (0);
 
- tortoise = head->next;
- hare = (head->next)->next;
+tortoise = head->next;
+hare = (head->next)->next;
 
- while (hare)
- {
- if (tortoise == hare)
- {
- tortoise = head;
- while (tortoise != hare)
- {
- nodes++;
- tortoise = tortoise->next;
- hare = hare->next;
- }
+while (hare)
+{
+if (tortoise == hare)
+{
+tortoise = head;
+while (tortoise != hare)
+{
+nodes++;
+tortoise = tortoise->next;
+hare = hare->next;
+}
 
- tortoise = tortoise->next;
- while (tortoise != hare)
- {
- nodes++;
- tortoise = tortoise->next;
- }
+tortoise = tortoise->next;
+while (tortoise != hare)
+{
+nodes++;
+tortoise = tortoise->next;
+}
 
- return (nodes);
- }
+return (nodes);
+}
 
- tortoise = tortoise->next;
- hare = (hare->next)->next;
- }
+tortoise = tortoise->next;
+hare = (hare->next)->next;
+}
 
  return (0);
 }
